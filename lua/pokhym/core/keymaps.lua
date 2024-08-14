d8e2a9aac6784d7e767a3c2391d7290440ad45a8
pokhym <ng121@purdue.edu>
2024-08-14 10:51:07 -0400

    [KEYMAPS] add get rel/full paths of open buffer, add get filename

--- lua/pokhym/core/keymaps.lua
+++ lua/pokhym/core/keymaps.lua
@@ -17,3 +17,8 @@ keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" 
 keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" }) --  go to next tab
 keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" }) --  go to previous tab
 keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" }) --  move current buffer to new tab
+
+-- Copy paths
+keymap.set("n", "<leader>pr", '<cmd>let @+ = expand("%")<CR>', { desc = "Copy relative path to unnamed register" })
+keymap.set("n", "<leader>pf", '<cmd>let @+ = expand("%:p")<CR>', { desc = "Copy full path to unnamed register" })
+keymap.set("n", "<leader>pn", '<cmd>let @+ = expand("%:t")<CR>', { desc = "Copy filename to unnamed register" })
