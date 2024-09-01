a1b0f53a70222b11701d612538ec0b2073840db1
pokhym <ng121@purdue.edu>
2024-09-01 03:13:23 -0400

    [ADD] Option to not use ripgrep

--- lua/pokhym/plugins/telescope.lua
+++ lua/pokhym/plugins/telescope.lua
@@ -26,6 +26,22 @@ return {
 						["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
 					},
 				},
+				-- -- Use grep wihtout rg dependency
+				-- vimgrep_arguments = {
+				-- 	"grep",
+				-- 	"--extended-regexp",
+				-- 	"--color=never",
+				-- 	"--with-filename",
+				-- 	"--line-number",
+				-- 	"-b", -- grep doesn't support a `--column` option :(
+				-- 	"--ignore-case",
+				-- 	"--recursive",
+				-- 	"--no-messages",
+				-- 	"--exclude-dir=*cache*",
+				-- 	"--exclude-dir=*.git",
+				-- 	"--exclude=.*",
+				-- 	"--binary-files=without-match"
+				-- },
 			},
 		})
 
