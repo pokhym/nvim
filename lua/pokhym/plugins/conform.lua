172cfbf8ceb7e81470cec7fe8e269db39ae23b1a
pokhym <ng121@purdue.edu>
2024-09-01 03:12:52 -0400

    [ADD] basic rust support

--- lua/pokhym/plugins/conform.lua
+++ lua/pokhym/plugins/conform.lua
@@ -6,20 +6,21 @@ return {
 	config = function()
 		local conform = require("conform")
 
-    conform.setup({
-      formatters_by_ft = {
-        markdown = { "prettier" },
-        lua = { "stylua" },
-        python = { "isort", "black" },
-        c = { "clang-format" },
-        cpp = { "clang-format" },
-      },
-      format_on_save = {
-        lsp_fallback = true,
-        async = false,
-        timeout_ms = 1000,
-      },
-    })
+		conform.setup({
+			formatters_by_ft = {
+				markdown = { "prettier" },
+				lua = { "stylua" },
+				python = { "isort", "black" },
+				c = { "clang-format" },
+				cpp = { "clang-format" },
+				rust = { "rustfmt", lsp_format = "fallback" },
+			},
+			format_on_save = {
+				lsp_fallback = true,
+				async = false,
+				timeout_ms = 1000,
+			},
+		})
 
 		vim.keymap.set({ "n", "v" }, "<leader>mp", function()
 			conform.format({
--- lua/pokhym/plugins/lsp/mason.lua
+++ lua/pokhym/plugins/lsp/mason.lua
@@ -1,48 +1,51 @@
 return {
-  "williamboman/mason.nvim",
-  dependencies = {
-    "williamboman/mason-lspconfig.nvim",
-    "WhoIsSethDaniel/mason-tool-installer.nvim",
-  },
-  config = function()
-    -- import mason
-    local mason = require("mason")
+	"williamboman/mason.nvim",
+	dependencies = {
+		"williamboman/mason-lspconfig.nvim",
+		"WhoIsSethDaniel/mason-tool-installer.nvim",
+	},
+	config = function()
+		-- import mason
+		local mason = require("mason")
 
-    -- import mason-lspconfig
-    local mason_lspconfig = require("mason-lspconfig")
-    local mason_tool_installer = require("mason-tool-installer")
+		-- import mason-lspconfig
+		local mason_lspconfig = require("mason-lspconfig")
+		local mason_tool_installer = require("mason-tool-installer")
 
-    -- enable mason and configure icons
-    mason.setup({
-      ui = {
-        icons = {
-          package_installed = "✓",
-          package_pending = "➜",
-          package_uninstalled = "✗",
-        },
-      },
-    })
+		-- enable mason and configure icons
+		mason.setup({
+			ui = {
+				icons = {
+					package_installed = "✓",
+					package_pending = "➜",
+					package_uninstalled = "✗",
+				},
+			},
+		})
 
-    mason_lspconfig.setup({
-      -- list of servers for mason to install
-      ensure_installed = {
-        "lua_ls",
-        "pyright",
-        "bashls",
-        "clangd",
-        "nim_langserver"
-      },
-    })
-    mason_tool_installer.setup({
-      ensure_installed = {
-        "pylint",         -- python linter
-        "cpplint",        -- c linter
-        "clang-format",   -- c formatter
-        "isort", "black", -- python formatter
-        "stylua",         -- lua formatter
-        "prettier",       -- markdown formatter
-      },
-    })
-  end,
-
+		mason_lspconfig.setup({
+			-- list of servers for mason to install
+			ensure_installed = {
+				"lua_ls",
+				"pyright",
+				"bashls",
+				"clangd",
+				"nim_langserver",
+				"rust_analyzer",
+			},
+		})
+		mason_tool_installer.setup({
+			ensure_installed = {
+				"pylint", -- python linter
+				"cpplint", -- c linter
+				"clang-format", -- c formatter
+				"isort",
+				"black", -- python formatter
+				"stylua", -- lua formatter
+				"prettier", -- markdown formatter
+				"rust-analyzer",
+				"codelldb",
+			},
+		})
+	end,
 }
